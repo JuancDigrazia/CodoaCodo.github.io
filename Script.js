43c11a89a74a304accbd631ee7c0f29022d279a2
juancruz <juanc.dra@gmail.com>
2023-06-11 12:03:03 -0300

    2.3

--- Script.js
+++ Script.js
@@ -7,29 +7,29 @@ let descuentoJunior = 0.15;
 const cantidad = document.querySelector('#NumCantidad');
 const categoria = document.querySelector('#categoriaSelect');
 const botonBorrar = document.querySelector('#borrar');
-const botonResumen = document.querySelectorAll("#PRECIO");
+const botonResumen = document.querySelectorAll('#PRECIO');
 const parrafo = document.querySelector('#parrafo');
 
 function totalPagar() {
   let totalValor = parseInt(cantidad.value) * valorTicket;
 
   if (categoria.value == 1) {
-    totalValor *= descuentoEstudiante;
+    totalValor = descuentoEstudiante;
   } else if (categoria.value == 2) {
-    totalValor *= descuentoTrainee;
+    totalValor = descuentoTrainee;
   } else if (categoria.value == 3) {
-    totalValor *= descuentoJunior;
+    totalValor = descuentoJunior;
   } else if (categoria.value == 0) {
-    totalValor *= 1; 
+    totalValor = valorTicket; 
   }
 
   parrafo.innerHTML = `Total a Pagar: $${totalValor}`;
 }
 
-botonResumen.addEventListener('click', totalPagar);
+botonResumen.addEventListener('click',totalPagar);
 // Error en el botonResumen.addEventListener que sale en la consola = Uncaught TypeError: Cannot read properties of null (reading 'addEventListener' at Script.js:29:14
 
-botonBorrar.addEventListener('click', () => {
-  parrafo.innerHTML = 'Total a Pagar:';
-  cantidad.value = 0;
-});
+botonBorrar.addEventListener('click',() => {
+    parrafo.innerHTML = 'Total a Pagar: $';
+    cantidad.value=0;
+})
